4730f5b7282987e5a849838cbd59e46e5cfff32a
Thomaz Machado <thomazcm@gmail.com>
2022-07-31 13:32:59 -0300

    first commit

---
+++ src/main/java/br/com/alura/mvc/mudi/controller/PedidoController.java
@@ -0,0 +1,34 @@
+package br.com.alura.mvc.mudi.controller;
+
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import br.com.alura.mvc.mudi.dto.RequisicaoNovoPedido;
+import br.com.alura.mvc.mudi.repository.PedidoRepository;
+
+@Controller
+@RequestMapping("pedido")
+public class PedidoController {
+    @Autowired
+    private PedidoRepository pedidoRepository;
+    
+    @GetMapping("formulario")
+    public String formulario(RequisicaoNovoPedido requisicaoNovoPedido) {
+        return "pedido/formulario";
+    }
+    
+    @PostMapping("novo")
+    public String novo(@Valid RequisicaoNovoPedido requisicaoNovoPedido, BindingResult result) {
+        if(result.hasErrors()) {
+            return "pedido/formulario";
+        }
+        pedidoRepository.save(requisicaoNovoPedido.toPedido());
+        return "redirect:/home";
+    }
+}
---
+++ src/main/java/br/com/alura/mvc/mudi/model/Pedido.java
@@ -0,0 +1,93 @@
+package br.com.alura.mvc.mudi.model;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Pedido {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    private String nomeProduto;
+    private BigDecimal valorNegociado;
+    private LocalDate dataEntrega;
+    private String urlProduto;
+    private String urlImagem;
+    private String descricao;
+    @Enumerated(EnumType.STRING)
+    private StatusPedido status;
+
+    public Pedido() {
+    }
+    
+    public Pedido(String nomeProduto, String urlProduto, String urlImagem, String descricao) {
+        this.nomeProduto = nomeProduto;
+        this.urlProduto = urlProduto;
+        this.urlImagem = urlImagem;
+        this.descricao = descricao;
+    }
+
+    public String getNomeProduto() {
+        return nomeProduto;
+    }
+
+    public void setNomeProduto(String nomeProduto) {
+        this.nomeProduto = nomeProduto;
+    }
+
+    public BigDecimal getValorNegociado() {
+        return valorNegociado;
+    }
+
+    public void setValorNegociado(BigDecimal valorNegociado) {
+        this.valorNegociado = valorNegociado;
+    }
+
+    public LocalDate getDataEntrega() {
+        return dataEntrega;
+    }
+
+    public void setDataEntrega(LocalDate dataEntrega) {
+        this.dataEntrega = dataEntrega;
+    }
+
+    public String getUrlProduto() {
+        return urlProduto;
+    }
+
+    public void setUrlProduto(String urlProduto) {
+        this.urlProduto = urlProduto;
+    }
+
+    public String getUrlImagem() {
+        return urlImagem;
+    }
+
+    public void setUrlImagem(String urlImagem) {
+        this.urlImagem = urlImagem;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public StatusPedido getStatus() {
+        return status;
+    }
+
+    public void setStatus(StatusPedido status) {
+        this.status = status;
+    }
+}
